473623910e5f1d8c133134b25ceb8c7520179f65
Nishanth <94614270+nishanth-ac@users.noreply.github.com>
2022-09-07 15:50:19 +0530

    Uploaded all source files and codes

---
+++ script.js
@@ -0,0 +1,34 @@
+const btn = document.getElementById('calculate');
+
+btn.addEventListener('click', function(){
+    let height = document.querySelector('#height').value;
+    let weight = document.querySelector('#weight').value;
+
+    if (height == '' ||weight == ''){
+        alert('Please fill out the input fields!');
+        return;
+    }
+
+    // BMI = weight in KG / height in m ^2 
+
+    height = height / 100
+    let BMI = (weight/(height*height));
+    BMI = Math.floor(BMI.toFixed(2))
+    document.querySelector('#result').innerHTML = BMI;
+    let status = '';
+    if(BMI < 18.5){
+        status = "Underweight";
+    }
+    if(BMI >= 18.5 && BMI <25){
+        status = "healthy";
+    }
+    if(BMI >= 25 && BMI <30){
+        status = "Overweight";
+    }
+    if(BMI >=30){
+        status = "Obese";
+    }
+    document.querySelector('.comment').innerHTML = `Comment: you are <span id="comment"> ${status}</span>`;
+
+    console.log(BMI);
+})
